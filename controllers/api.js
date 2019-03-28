1459d56fec1522f265d98c617f66f90e2a7a6ec2
sevendc <benford.mark783@gmail.com>
2019-03-28 20:50:19 +0400

    Attempt to integrate redis

--- controllers/api.js
+++ controllers/api.js
@@ -8,6 +8,7 @@ var redis = new Redis(process.env.REDIS_URL,{connectTimeout: 10000});
 
 // TODO
 // 2. fetch img's from json then convert it to binary and save it in db instead url from wolfram
+  // Another option - Integrate MathJax library for formating solutions
 // 3. media query for resize img in solution
 // 4. Delete all solutions
 // 5. Redis integration
@@ -18,29 +19,39 @@ var redis = new Redis(process.env.REDIS_URL,{connectTimeout: 10000});
 // Redis test route /redis
 exports.testRedis = (req, res) => {
 
-async function main() {
-  const shamu = {
-      type: 'killer whale',
-      age: 5,
-      lastFeedDate: 'Jan 06 2018',
-      size: { length: 30, weight: 8 },
-  };
+  async function main() {
+    const shamu = {
+        // type: 'killer whale',
+        // age: 5,
+        // lastFeedDate: 'Jan 06 2018',
+        solution: 'name'
+    };
 
-  try {
-      const key = 'shamu';
-      const result = await redis.set(key, JSON.stringify(shamu));
-      //console.log(result);
-      // Turn around and bring back Shamu immediately to prove it works.
-      const shamuReturns = JSON.parse(await redis.get(key));
-      console.log(shamuReturns);
-      res.redirect('/');
-  } catch (error) {
-      console.error(error);
-  }
+    try {
+        const key = 'shamu';
+        const result = await redis.hmset(key, shamu);
+        console.log(result);
+        //HMGET myhash field1 field2 nofield (for multiple values)
+        //HGET for single value
+
+        //For retrive all values example
+        // redisclient.hgetall(key, function (err, dbset) {
+
+        //   // gather all records
+        //   for (id in dbset) {
+        //        ...
+        //   }
+        // }); 
+        const rcache = await redis.hget('shamu', 'solution');
+        console.log(rcache);
+    }
+    catch (error) {
+        console.error(error);
+    }
+    //redis.disconnect();
 }
 
 main();
-// });
 }
 // Get api index page
 exports.getApi = (req, res) => {
@@ -72,6 +83,7 @@ exports.deleteAllSolutions = (req, res) => {
 
 // Show single solution route = solution/:id
 exports.getSolution = (req, res) => {
+  const key = 
   Task.findOne({
     _id: req.params.id
   })
@@ -144,8 +156,62 @@ exports.getPDFSolution = (req, res) => {
 
 };
 
+// 1. Check in cache (if exists then redirect to solution)
+//  1.1 Get hash with solution name as key and _id solution as value ex. 
+//    (integrate e^x/(e^(2x)+2e^x+1): '5c9a7d6bb4fec20c10e716db')
+//  1.2 Get necessary value (_id) from hash and using it as key for search solution from cache. 
+// 2. Check in db (if exists then save it to cache and redirect)
+//  2.1 Add to hash new key: value
+// 3. Send request to WolframAlpha 
+// 4. Save it to db
+// 5. Save it to cache
+//  5.1 Create hash (if not exists), then add key(taksName): value(_id)
+//  5.2 Create solution record in Redis as key(id): value(solution)
+// 6. Redirect to solution
+
 // Find solution and save it
 // integrate e^x/(e^(2x)+2e^x+1)
+// exports.findSolution = (req, res) => {
+//   const key = req.body.task;
+//   const cachedSolution = JSON.parse(redis.get(key));
+//   if (cachedSolution) {
+//     console.log(`Exists in cache. Redirecting to solution ${cachedSolution.taskName}`);
+//     res.redirect(`api/solution/${cachedSolution.id}`);
+//   } else {
+//     Task.findOne({
+//       taskName: req.body.task
+//     })
+//     .then(solutionFromDB => {
+//       if(solutionFromDB) {
+//         const result = redis.set(solutionFromDB.taskName, JSON.stringify(solutionFromDB));
+//         console.log(`Status Redis: ${result}. Exists in db. Saved to cache and redirecting to ${solutionFromDB.taskName}`);
+//         res.redirect(`api/solution/${solutionFromDB.id}`);
+//       }
+//       else {
+//         const waApi = WolframAlphaAPI(process.env.WOLFRAM_KEY);
+//         const waTask = req.body.task;
+//         waApi.getFull(waTask).then((queryresult) => {
+//         const pods = queryresult.pods;
+//         const newSolution = {
+//           taskName: req.body.task,
+//           contentData: pods,
+//           user: req.user.id
+//         }
+//         // Save to db
+//           new Task(newSolution)
+//             .save()
+//             console.log('New solution was added in db...')
+//             .then(task => {
+//               const cacheSolution = redis.set(task.taskName, JSON.stringify(task))
+//               console.log(`Status Redis: ${cacheSolution}. Saved to cache and redirecting to ${task.taskName}`)
+//               res.redirect(`api/solution/${task.id}`);
+//             });
+//       }).catch(console.error);
+//       }
+//     });
+//   }
+// };
+
 exports.findSolution = (req, res, next) => {
   Task.findOne({
     taskName: req.body.task
--- test.js
+++ test.js
@@ -34,3 +34,216 @@ waApi.getFull('integrate e^x/(e^(2x)+2e^x+1)').then((queryresult) => {
 //   }).join('\n');
 //   console.log(output);
 // }).catch(console.error);
+
+exports.findSolution = (req, res) => {
+  const key = req.body.task;
+  const cachedSolution = JSON.parse(redis.get(key));
+  if (cachedSolution) {
+    console.log(`Exists in cache. Redirecting to solution ${cachedSolution.taskName}`);
+    res.redirect(`api/solution/${cachedSolution.id}`);
+  } else {
+    Task.findOne({
+      taskName: req.body.task
+    })
+    .then(solutionFromDB => {
+      if(solutionFromDB) {
+        const result = redis.set(solutionFromDB.taskName, JSON.stringify(solutionFromDB));
+        console.log(`Status Redis: ${result}. Exists in db. Saved to cache and redirecting to ${solutionFromDB.taskName}`);
+        res.redirect(`api/solution/${solutionFromDB.id}`);
+      }
+      else {
+        const waApi = WolframAlphaAPI(process.env.WOLFRAM_KEY);
+        const waTask = req.body.task;
+        waApi.getFull(waTask).then((queryresult) => {
+        const pods = queryresult.pods;
+        const newSolution = {
+          taskName: req.body.task,
+          contentData: pods,
+          user: req.user.id
+        }
+        // Save to db
+          new Task(newSolution)
+            .save()
+            console.log('New solution was added in db...')
+            .then(task => {
+              const cacheSolution = redis.set(task.taskName, JSON.stringify(task))
+              console.log(`Status Redis: ${cacheSolution}. Saved to cache and redirecting to ${task.taskName}`)
+              res.redirect(`api/solution/${task.id}`);
+            });
+      }).catch(console.error);
+      }
+    });
+  }
+};
+
+
+// Promise Example 
+/* ES5, using Bluebird */
+var isMomHappy = true;
+
+// Promise
+var willIGetNewPhone = new Promise(
+    function (resolve, reject) {
+        if (isMomHappy) {
+            var phone = {
+                brand: 'Samsung',
+                color: 'black'
+            };
+            resolve(phone);
+        } else {
+            var reason = new Error('mom is not happy');
+            reject(reason);
+        }
+
+    }
+);
+
+
+// call our promise
+var askMom = function () {
+    willIGetNewPhone
+        .then(function (fulfilled) {
+            // yay, you got a new phone
+            console.log(fulfilled);
+        })
+        .catch(function (error) {
+            // ops, mom don't buy it
+            console.log(error.message);
+        });
+}
+
+askMom();
+
+// 2й промис
+
+var showOff = function (phone) {
+
+  var message = 'Hey friend, I have a new ' +
+  
+    phone.color + ' ' + phone.brand + ' phone';
+  
+  return Promise.resolve(message);
+  
+  };
+
+  // Вызываем промис
+var askMom = function () {
+  willIGetNewPhone
+  .then(showOff) // связываем
+  .then(function (fulfilled) {
+          console.log(fulfilled);
+       // output: 'Hey friend, I have a new black Samsung phone.'
+      })
+      .catch(function (error) {
+          // oops, mom don't buy it
+          console.log(error.message);
+       // output: 'mom is not happy'
+      });
+};
+var askMom = function () {
+  console.log('before asking Mom'); // Выводим в консоль до
+  willIGetNewPhone
+      .then(showOff)
+      .then(function (fulfilled) {
+          console.log(fulfilled);
+      })
+      .catch(function (error) {
+          console.log(error.message);
+      });
+  console.log('after asking mom'); // Выводим в консоль после
+}
+// 1. before asking Mom
+
+// 2. after asking mom
+
+// 3. Hey friend, I have a new black Samsung phone.
+
+//Все что должно подождать промиса перед выполнением, вы вставляете в .then .
+
+// /_ ES6 _/
+const isMomHappy = true;
+
+// Промис
+const willIGetNewPhone = new Promise(
+    (resolve, reject) => { // fat arrow
+        if (isMomHappy) {
+            const phone = {
+                brand: 'Samsung',
+                color: 'black'
+            };
+            resolve(phone);
+        } else {
+            const reason = new Error('mom is not happy');
+            reject(reason);
+        }
+
+    }
+);
+
+const showOff = function (phone) {
+    const message = 'Hey friend, I have a new ' +
+                phone.color + ' ' + phone.brand + ' phone';
+    return Promise.resolve(message);
+};
+
+// Вызываем промис
+const askMom = function () {
+    willIGetNewPhone
+        .then(showOff)
+        .then(fulfilled => console.log(fulfilled)) // fat arrow
+        .catch(error => console.log(error.message)); // fat arrow
+};
+
+askMom();
+
+// /_ ES7 _/
+const isMomHappy = true;
+
+// Промис
+const willIGetNewPhone = new Promise(
+    (resolve, reject) => {
+        if (isMomHappy) {
+            const phone = {
+                brand: 'Samsung',
+                color: 'black'
+            };
+            resolve(phone);
+        } else {
+            const reason = new Error('mom is not happy');
+            reject(reason);
+        }
+
+    }
+);
+
+// 2й промис
+async function showOff(phone) {
+    return new Promise(
+        (resolve, reject) => {
+            var message = 'Hey friend, I have a new ' +
+                phone.color + ' ' + phone.brand + ' phone';
+
+            resolve(message);
+        }
+    );
+};
+
+// Вызываем промис
+async function askMom() {
+    try {
+        console.log('before asking Mom');
+
+        let phone = await willIGetNewPhone;
+        let message = await showOff(phone);
+
+        console.log(message);
+        console.log('after asking mom');
+    }
+    catch (error) {
+        console.log(error.message);
+    }
+}
+
+(async () => {
+    await askMom();
+})();
